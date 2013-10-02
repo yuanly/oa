6dbada8efe5ce5005a4043efb8a6ddbefcf3094a
yuanly <27408136@qq.com>
2013-10-03 02:25:44 +0800

    1

---
+++ mainframe/center/yuangao/yuangao.php
@@ -0,0 +1,13 @@
+﻿<?php
+//error_log("Location: ".docRoot()."login/login.html",3,"d:/err.log");
+
+include("../../../util.php");
+session_start();
+checkuser();
+
+$yangban = getJson();
+$yangban["access"] = time();
+coll("yangban")->save($yangban);
+$liuyan = array("_id"=>time(),"hostType"=>"yangban","hostId"=>$yangban["_id"],"type"=>"caozuorizhi","userId"=>(String)$_SESSION["user"]["_id"],"neirong"=>"提交：".jsonEncode($yangban));
+coll("liuyan")->save($liuyan);
+echo '{"success":true}';
---
+++ mainframe/center/yuangao/yuangaos.php
@@ -0,0 +1,35 @@
+﻿<?php
+//error_log("Location: ".docRoot()."login/login.html",3,"d:/err.log");
+
+include("../../../util.php");
+session_start();
+checkuser();
+
+$param = getJson();
+if(isset($param["_id"])){//
+	$one = coll("yangban")->findAndModify(array("_id"=>$param["_id"]),array('$set'=>Array("access"=>time())));			
+	if($one){
+		$cur = coll("yangban")->find(array("zhongguoxinghao"=>$one["zhongguoxinghao"]),array("_id"=>1,"shangjia"=>1,"jiage"=>1));
+		if($cur->count()>1){
+			$one["beixuan"] = c2a($cur);
+		}
+	}
+	echo jsonEncode($one);
+}else{//
+	$query = array();
+	if(!empty($param["option"])){
+		if(!empty($param["option"]["bianhao"])){
+			$query["_id"] = array('$regex'=>$param["option"]["bianhao"]);
+		}else if(!empty($param["option"]["shangjia"])){
+			$query["shangjia.mingchen"] = array('$regex'=>$param["option"]["shangjia"]);
+		}else if(!empty($param["option"]["taixing"])){
+			$query["taiguoxinghao"] = array('$regex'=>$param["option"]["taixing"]);
+		}else if(!empty($param["option"]["zhongxing"])){
+			$query["zhongguoxinghao"] = array('$regex'=>$param["option"]["zhongxing"]);
+		}
+	}
+	$cur = coll("yangban")->find($query,array("beizhu"=>0))->sort(array("access"=>-1))->skip($param["offset"])->limit($param["limit"]);
+	
+	echo  cur2json($cur);
+	
+}
