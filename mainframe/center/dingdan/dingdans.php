af553971c28ad41752881f8784da48f0dd667468
yuanly <27408136@qq.com>
2013-10-22 12:31:45 +0800

    4

---
+++ mainframe/center/dingdan/dingdans.php
@@ -0,0 +1,29 @@
+﻿<?php
+//error_log("Location: ".docRoot()."login/login.html",3,"d:/err.log");
+
+include("../../../util.php");
+session_start();
+checkuser();
+
+$param = getJson();
+if(isset($param["_id"])){//
+	$one = coll("yuangao")->findOne(array("_id"=>$param["_id"]));
+	echo jsonEncode($one);
+}else{//
+	$query = array("zhuangtai"=>array('$in'=>array("上传","接稿","申请审结","审结")));//排除“删除”
+	if("jiegao" == $param["option"]["cmd"]){
+		$query = array("zhuangtai"=>"上传");
+	}else if("ludan" == $param["option"]["cmd"]){
+		$query = array("zhuangtai"=>"接稿","jiegaozhe"=>(int)$_SESSION["user"]["_id"]);
+	}else if("shenjie" == $param["option"]["cmd"]){
+		$query = array("zhuangtai"=>"申请审结","jiegaozhe"=>array('$ne'=>(int)$_SESSION["user"]["_id"]));
+	}else if($param["option"]["weishenjie"]){
+		$query = array("zhuangtai"=>array('$in'=>array("上传","接稿","申请审结")));//排除“删除”和“审结”
+	}else if($param["option"]["shangchuanshijian"]){
+		$query["shangchuanshijian"] = array('$lte'=>$param["option"]["shangchuanshijian"]);
+	}
+	$cur = coll("yuangao")->find($query,array("neirong"=>0,"shenjieshuoming"=>0))->sort(array("_id"=>-1))->skip($param["offset"])->limit($param["limit"]);
+	
+	echo  cur2json($cur);
+	
+}
