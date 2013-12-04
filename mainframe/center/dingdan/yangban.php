913fee319df65ce284349465d164e878392ae69c
yuanly <27408136@qq.com>
2013-12-05 02:22:54 +0800

    3

---
+++ mainframe/center/dingdan/yangban.php
@@ -0,0 +1,14 @@
+<?php
+//error_log("Location: ".docRoot()."login/login.html",3,"d:/err.log");
+
+include("../../../util.php");
+session_start();
+checkuser();
+
+$param = getJson();
+$query = array();
+$query["taiguoxinghao"] = array('$regex'=>$param["option"]);
+$cur = coll("yangban")->find($query,array("beizhu"=>0))->sort(array("access"=>-1))->skip($param["offset"])->limit($param["limit"]);
+
+echo  cur2json($cur);
+	
