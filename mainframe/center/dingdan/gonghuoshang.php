a037df5bfd5efdd23e47233b9827ad882e17ad21
yuanly <27408136@qq.com>
2013-12-10 02:40:15 +0800

    c

---
+++ mainframe/center/dingdan/gonghuoshang.php
@@ -0,0 +1,14 @@
+﻿<?php
+//error_log("Location: ".docRoot()."login/login.html",3,"d:/err.log");
+
+include("../../../util.php");
+session_start();
+checkuser();
+
+$param = getJson();
+$query = array();
+$query["mingchen"] = array('$regex'=>$param["option"]);
+$cur = coll("vendor")->find($query,array("beizhu"=>0))->sort(array("access"=>-1))->skip($param["offset"])->limit($param["limit"]);
+
+echo  cur2json($cur);
+	
