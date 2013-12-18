091306af23f88338e08cc61367855be9d90e2e7d
yuanly <27408136@qq.com>
2013-12-19 02:45:49 +0800

    2

---
+++ mainframe/center/fahuodan/fahuodan.php
@@ -0,0 +1,22 @@
+﻿<?php
+//error_log("Location: ".docRoot()."login/login.html",3,"d:/err.log");
+
+include("../../../util.php");
+session_start();
+checkuser();
+
+$param = getJson();
+if("shangchuan" == $param["caozuo"]){
+	$shangchuanliucheng = array("userId"=>(int)$_SESSION["user"]["_id"],"dongzuo"=>"上传","time"=>time());
+	$fahuodan = $param["fahuodan"];
+	$fahuodan["liucheng"][] = $shangchuanliucheng;
+	$d = "FHD".date("ymd",time());
+	$n = coll("fahuodan")->count(array("_id"=>array('$regex'=>"^".$d."")));
+	$fahuodan["_id"] = $d.".".($n+1);
+	coll("fahuodan")->save($fahuodan);
+	echo '{"success":true}';
+}else if("chaxun" == $param["caozuo"]){
+	$query = array();
+	$cur = coll("fahuodan")->find($query)->sort(array("_id"=>-1))->skip($param["offset"])->limit($param["limit"]);
+	echo  cur2json($cur);
+}
