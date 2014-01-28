26e3d4ca449b0b6897a6e43fb7b3b4242e9e3ad1
yuanly <27408136@qq.com>
2014-01-28 17:30:39 +0800

    b

---
+++ mainframe/center/contact/lianxiren.php
@@ -0,0 +1,13 @@
+﻿<?php
+//error_log("Location: ".docRoot()."login/login.html",3,"d:/err.log");
+
+include("../../../util.php");
+session_start();
+checkuser();
+
+$param = getJson();
+if("chashangjia" == $param["caozuo"]){
+	$query = array("leixing"=>"商家");
+	$cur = coll("contact")->find($query)->sort(array("_id"=>-1))->skip($param["offset"])->limit($param["limit"]);
+	echo  cur2json($cur);
+}
