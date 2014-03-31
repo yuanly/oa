07658b7ab2311e232e2c8a42baa89a67bb15d417
yuanly <27408136@qq.com>
2014-03-31 18:05:50 +0800

    a

--- mainframe/west/menu/menu.js
+++ mainframe/west/menu/menu.js
@@ -9,21 +9,22 @@ $(function(){
 	function stat(){
 		postJson("stat.php",{version:version},function(res){//·şÎñÆ÷
 			if(res.version > version){
-				$("#yuangaotip").text(res.yuangao>0?res.yuangao:"");
-				$("#dingdantip").text(res.dingdan>0?res.dingdan:"");
-				$("#dingdantip2").text(res.dingdan2>0?res.dingdan2:"");
-				$("#fahuodantip").text(res.fahuodan>0?res.fahuodan:"");
-				$("#fahuodantip2").text(res.fahuodan2>0?res.fahuodan2:"");
-				$("#yanhuodantip").text(res.yanhuodan>0?res.yanhuodan:"");
-				$("#yanhuodantip2").text(res.yanhuodan2>0?res.yanhuodan2:"");
-				$("#zhuangguidantip").text(res.zhuangguidan>0?res.zhuangguidan:"");
-				$("#zhuangguidantip2").text(res.zhuangguidan2>0?res.zhuangguidan2:"");
-				$("#jizhangtip").text(res.jizhang>0?res.jizhang:"");
-				$("#tuishuitip").text(res.tuishui>0?res.tuishui:"");
+				$("#yuangaotip").text(res.yuangao>0?res.yuangao:"-");
+				$("#dingdantip").text(res.dingdan>0?res.dingdan:"-");
+				$("#dingdantip2").text(res.dingdan2>0?res.dingdan2:"-");
+				$("#fahuodantip").text(res.fahuodan>0?res.fahuodan:"-");
+				$("#fahuodantip2").text(res.fahuodan2>0?res.fahuodan2:"-");
+				$("#yanhuodantip").text(res.yanhuodan>0?res.yanhuodan:"-");
+				$("#yanhuodantip2").text(res.yanhuodan2>0?res.yanhuodan2:"-");
+				$("#zhuangguidantip").text(res.zhuangguidan>0?res.zhuangguidan:"-");
+				$("#zhuangguidantip2").text(res.zhuangguidan2>0?res.zhuangguidan2:"-");
+				$("#fukuanshenqingtip").text(res.fukuanshenqing>0?res.fukuanshenqing:"-");
+				$("#jizhangtip").text(res.jizhang>0?res.jizhang:"-");
+				$("#tuishuitip").text(res.tuishui>0?res.tuishui:"-");
 				version = res.version;
 			}
 		});
 	}
 	stat();
-	//setInterval(stat,3000);
+	setInterval(stat,3000);
 });
--- mainframe/west/menu/stat.php
+++ mainframe/west/menu/stat.php
@@ -12,15 +12,16 @@ $stat = coll("config")->findOne(array("_id"=>"stat"));
 if(empty($stat) || $stat["expired"] ){//更新stat
 	$yuangao = coll("yuangao")->count(array("zhuangtai"=>array('$in'=>array("上传","接稿","申请审结"))));
 	$dingdan = coll("dingdan")->count(array("zhuangtai"=>"审核"));
-	$dingdan2 = coll("dingdan")->count(array("zhuangtai"=>array('$in'=>array("录单","审核","接单","等版","子单","下单","审单"))));
-	$fahuodan = coll("fanhuodan")->count(array("zhuangtai"=>"申请对单"));
-	$fahuodan2 = coll("fanhuodan")->count(array("zhuangtai"=>array('$in'=>array("申请对单","对单","付款","发货"))));
+	$dingdan2 = coll("dingdan")->count(array("zhuangtai"=>array('$in'=>array("录单","申请审核","审核","接单","作废","下单申请","下单审核","下单","发货","申请审结"))));
+	$fahuodan = coll("fanhuodan")->count(array("type"=>"fahuodan","zhuangtai"=>"申请对单"));
+	$fahuodan2 = coll("fanhuodan")->count(array("type"=>"fahuodan","zhuangtai"=>array('$in'=>array("上传","接单","作废","申请对单","对单","收货","装柜","申请审结"))));
 	$yanhuodan = coll("yanhuodan")->count(array("zhuangtai"=>"申请审核"));
-	$yanhuodan2 = coll("yanhuodan")->count(array("zhuangtai"=>array('$in'=>array("新建","申请受理","受理","申请审核"))));
-	$zhuangguidan = coll("zhuangguidan")->count(array("zhuangtai"=>"申请审核"));
-	$zhuangguidan2 = coll("zhuangguidan")->count(array("zhuangtai"=>array('$in'=>array("制单","交单","申请审核"))));
-	$jizhang = coll("liushuizhang")->count(array("zhuangtai"=>"申请审核"));
-	$tuishui = coll("tuishui")->count(array("zhuangtai"=>array('$in'=>array("新建","装柜","报关","付款","开票","单证"))));
+	$yanhuodan2 = coll("yanhuodan")->count(array("zhuangtai"=>array('$in'=>array("制单","申请受理","受理","申请审核"))));
+	$zhuangguidan = coll("zhuangguidan")->count(array("zhuangtai"=>"交单"));
+	$zhuangguidan2 = coll("zhuangguidan")->count(array("zhuangtai"=>array('$in'=>array("制单","交单"))));
+	$fukuanshenqing = coll("fahuodan")->count(array("typpe"=>"shenqing","zhuangtai"=>array('$in'=>array("制单","申请对单"))));
+	$jizhang = coll("liushuizhang")->count(array("zhuangtai"=>array('$in'=>array("记账","付款","作废","申请复核"))));
+	$tuishui = coll("tuishui")->count(array("zhuangtai"=>array('$in'=>array("新建","装柜","报关","付款","开票","单证","退税","申请复核"))));
 	$stat = array("yuangao"=>$yuangao,
 								"dingdan"=>$dingdan,
 								"dingdan2"=>$dingdan2,
@@ -30,6 +31,7 @@ if(empty($stat) || $stat["expired"] ){//更新stat
 								"yanhuodan2"=>$yanhuodan2,
 								"zhuangguidan"=>$zhuangguidan,
 								"zhuangguidan2"=>$zhuangguidan2,
+								"fukuanshenqing"=>$fukuanshenqing,
 								"jizhang"=>$jizhang,
 								"tuishui"=>$tuishui,
 								"version"=>time(),
